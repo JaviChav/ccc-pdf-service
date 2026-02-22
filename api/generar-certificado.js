122bc1b4d540e38ebacc6606d7b928e82579dedd
JaviChav <ejcha87@gmail.com>
2026-02-22 17:31:20 -0300

    Fix generar certificado API

--- api/generar-certificado.js
+++ api/generar-certificado.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
 
     await browser.close();
 
-    const fileName = `certificados/${certificado_id}.pdf`;
+    const fileName = `${certificado_id}.pdf`;
 
     // Subir PDF a Supabase Storage
     const { error: uploadError } = await supabaseAdmin.storage
